49c230f45a5b3c649bc10b0c3042349b808e245d
danielabaptista <m.danielabap@gmail.com>
2018-04-26 15:56:33 -0430

    cambiando mayusculas por minusculas en los codigos

--- sincronizacion/programa2.c
+++ sincronizacion/programa2.c
@@ -1,9 +1,9 @@
 
-/* Universidad de Los Andes
- * Sincronizacion de procesos
- * Asignatura: Sistemas Operativos
- * Autor: Alvaro Araujo
- * Fecha: 20/04/2018
+/* universidad de los andes
+ * sincronizacion de procesos
+ * asignatura: sistemas operativos
+ * autor: alvaro araujo
+ * fecha: 20/04/2018
  */
 
 #include <stdio.h>
@@ -32,7 +32,7 @@ int main()
   int sem;
 
   system("clear");
-  printf("\n\t--> Proceso %d \n\n",getpid());
+  printf("\n\t--> proceso %d \n\n",getpid());
   srand(getpid());
   key_t id_shmem = ftok(ROUTE, ID);
   void *pto_shmem;
@@ -69,7 +69,7 @@ int main()
 		  exit(EXIT_FAILURE);
 	  }
 
-    printf("\t\tMonitor sin espacio!!!\n\n");
+    printf("\t\tmonitor sin espacio!!!\n\n");
     exit(EXIT_SUCCESS);
   }
 
@@ -79,7 +79,7 @@ int main()
   for(i=0; i<repeticion; i++)
   {
     pto_inf->array_p[pos].numero++;
-    printf("\tNumero: %d\n",i);
+    printf("\tnumero: %d\n",i);
     usleep(500000);
   }
 
